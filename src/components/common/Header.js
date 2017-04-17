3205de245a2a9a2ba25c90499470a889e1bdf4fd
Feng Shang <fshang@apple.com>
2017-04-17 12:54:51 -0700

    css tweeak

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,14 +4,16 @@ import LoadingDots from './LoadingDots';
 
 const Header = ({loading}) => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
+    <div className="app-header">
+      <nav>
+        <IndexLink to="/" activeClassName="active">Home</IndexLink>
+        {" | "}
+        <Link to="/courses" activeClassName="active">Courses</Link>
+        {" | "}
+        <Link to="/about" activeClassName="active">About</Link>
+        {loading && <LoadingDots interval={100} dots={20}/>}
+      </nav>
+    </div>
   );
 };
 
