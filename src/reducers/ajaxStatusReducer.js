963ebb2ba5bad58c85b653d93894f75c57621d8b
Feng Shang <fshang@apple.com>
2017-04-17 22:43:45 -0700

    code clean

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -6,7 +6,6 @@ function actionTypeEndsInSuccess(type) {
 }
 
 export default function ajaxStatusReducer(state = initialState.ajaxCallInProgress, action) {
-  console.log(action.type);
   if (action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
   } else if (action.type == types.AJAX_CALL_ERROR || actionTypeEndsInSuccess(action.type)) {
